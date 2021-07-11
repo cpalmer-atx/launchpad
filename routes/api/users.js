520c3e7c43a0373a0be48808db295cd9cb99ef5f
Chad Palmer <cpalmer-atx@Chads-MBP.attlocal.net>
2021-07-11 01:39:17 -0500

    simple User model added

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../../models/User');
+
+// @route   GET api/users
+// @desc    Get all users
+// @access  Public
+router.get('/', (req, res) => {
+    return res.status(200).json({ msg: "get all users" });
+});
+
+module.exports = router;
