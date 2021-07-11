313b2f9cd20897555e922be366f9f478dbc28f99
Chad Palmer <cpalmer-atx@Chads-MBP.attlocal.net>
2021-07-11 01:38:06 -0500

    mongoDB connection and sample config file added

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = process.env.MONGO_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser:true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+        console.log('Successfully connected to MongoDB');
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
